184f23f75dac714c40b72041d36cfba1d31f895f
CAI\putosaa <andrewbrian.putosa@cai.io>
2020-10-27 13:08:45 +0800

    spring boot

---
+++ src/main/java/com/andrewbrianputosa/bikerentalshop/BikerentalshopApplication.java
@@ -0,0 +1,13 @@
+package com.andrewbrianputosa.bikerentalshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikerentalshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikerentalshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrewbrianputosa/bikerentalshop/BikerentalshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewbrianputosa.bikerentalshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikerentalshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
